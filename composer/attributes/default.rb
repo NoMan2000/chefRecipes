80af49ece6a7862aa2632a2fdeaa4dd4261f507f
Michael Ryan Soileau <webart.video@gmail.com>
2015-01-27 10:53:33 -0600

    Update default.rb
    Adding in handlers for autoloading and updating.

--- composer/attributes/default.rb
+++ composer/attributes/default.rb
@@ -30,3 +30,17 @@ composer_project "/srv/www/cellcontrol/current" do
     prefer_dist false
     action :install
 end
+
+#update project vendors
+composer_project "/srv/www/cellcontrol/current" do
+    dev false
+    quiet true
+    action :update
+end
+
+#dump-autoload for project
+composer_project "/srv/www/cellcontrol/current" do
+    dev false
+    quiet true
+    action :dump_autoload
+end
